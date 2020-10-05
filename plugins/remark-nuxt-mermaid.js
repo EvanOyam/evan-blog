5a86403b662c08bc43e7be35b48e002e91871c5c
Evan <evan.ouyang@mart.blue>
2020-10-05 22:28:40 +0800

    feat(mermaid): markdown新增支持mermaid流程图和时序图

--- plugins/remark-nuxt-mermaid.js
+++ plugins/remark-nuxt-mermaid.js
@@ -7,43 +7,44 @@ const visit = require('unist-util-visit')
 const PLUGIN_NAME = 'remark-nuxt-mermaid'
 
 /**
- * Accepts the `source` of the graph as a string, and render an SVG using
- * mermaid.cli. Returns the path to the rendered SVG.
+ * 接收 mermaid 风格的 md 源码，利用 mermaid.cli 绘制 svg
+ * 返回 svg 的编码
  *
  * @param  {string} source
- * @param  {string} dirPath
  * @return {string}
  */
-function render(source, dirPath) {
+function render(source) {
   const unique = crypto
     .createHmac('sha1', PLUGIN_NAME)
     .update(source)
     .digest('hex')
-  const mmdPath = path.join(dirPath, `${unique}.mmd`)
-  const svgFilename = `${unique}.svg`
-  const svgPath = path.join(dirPath, svgFilename)
+  const mmdPath = path.join(__dirname, `${unique}.mmd`)
+  const svgPath = path.join(__dirname, `${unique}.svg`)
 
-  // Write temporary file
-  fs.outputFileSync(mmdPath, source)
+  let svgSource
+  try {
+    // 生成 mmd 文件
+    fs.outputFileSync(mmdPath, source)
 
-  // Invoke mermaid.cli
-  execSync(`mmdc -i ${mmdPath} -o ${svgPath} -b transparent`)
+    // mermaid.cli 生成 svg 文件
+    execSync(`mmdc -i ${mmdPath} -o ${svgPath} -b transparent`)
+    svgSource = fs.readFileSync(svgPath, 'utf-8')
 
-  // Clean up temporary file
-  fs.removeSync(mmdPath)
+    // 清空临时文件
+    fs.removeSync(mmdPath)
+    fs.removeSync(svgPath)
+  } catch (error) {
+    console.error('Error!!! Render SVG Rendering Failed')
+  }
 
-  return `/md/${svgFilename}`
+  return svgSource
 }
 
 /**
- * Returns the transformer which acts on the MDAST tree and given VFile.
+ * 返回作用于MDAST树和给定VFile的转换器
  *
- * If `options.simple` is passed as a truthy value, the plugin will convert
- * to `<div class="mermaid">` rather than a SVG image.
- *
- * @link https://github.com/unifiedjs/unified#function-transformernode-file-next
- * @link https://github.com/syntax-tree/mdast
- * @link https://github.com/vfile/vfile
+ * @link https://github.com/syntax-tree/unist-util-visit
+ * @link https://github.com/syntax-tree/mdast#html
  *
  * @return {function}
  */
@@ -56,29 +57,21 @@ function mermaid() {
   return function transformer(ast, vFile) {
     visit(ast, 'code', (node, index, parent) => {
       const { lang, value } = node
-      const dirPath = path.join(__dirname, '../static/md')
-
-      // If this codeblock is not mermaid, bail.
       if (lang !== 'mermaid') return node
 
-      let graphSvgFilename
+      let svgSource
       try {
-        graphSvgFilename = render(value, dirPath)
+        svgSource = render(value)
         vFile.info(`${lang} code block replaced with graph`, PLUGIN_NAME)
+        const newNode = {
+          type: 'html',
+          value: svgSource,
+        }
+        parent.children.splice(index, 1, newNode)
       } catch (error) {
         vFile.message(error, PLUGIN_NAME)
         return node
       }
-
-      const newNode = {
-        type: 'image',
-        title: '`mermaid` image',
-        url: graphSvgFilename,
-      }
-
-      parent.children.splice(index, 1, newNode)
-
-      return node
     })
     return ast
   }
